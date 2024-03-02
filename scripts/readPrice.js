75c30805d75795c44d0692eb0d5a795f1b0589c5
didoshotev <didoshotev@gmail.com>
2024-03-02 19:41:56 +0200

    feat: read price script

--- scripts/readPrice.js
+++ scripts/readPrice.js
@@ -2,14 +2,73 @@
 // yarn hardhat node
 // yarn hardhat run scripts/readPrice.js --network localhost
 const { ethers } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+const aggregatorV3InterfaceABI = [
+    {
+        inputs: [],
+        name: "decimals",
+        outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "description",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+        name: "getRoundData",
+        outputs: [
+            { internalType: "uint80", name: "roundId", type: "uint80" },
+            { internalType: "int256", name: "answer", type: "int256" },
+            { internalType: "uint256", name: "startedAt", type: "uint256" },
+            { internalType: "uint256", name: "updatedAt", type: "uint256" },
+            { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "latestRoundData",
+        outputs: [
+            { internalType: "uint80", name: "roundId", type: "uint80" },
+            { internalType: "int256", name: "answer", type: "int256" },
+            { internalType: "uint256", name: "startedAt", type: "uint256" },
+            { internalType: "uint256", name: "updatedAt", type: "uint256" },
+            { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "version",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+    },
+]
 
-async function readPrice() {
-    const priceConsumerV3 = await ethers.getContract("PriceConsumerV3")
-    const price = await priceConsumerV3.getLatestPrice()
-    console.log(price.toString())
+async function readPriceFromAggregator() {
+    try {
+        const [deployer] = await ethers.getSigners()
+
+        const priceFeed = new ethers.Contract(networkConfig[56].ethUsdPriceFeed, aggregatorV3InterfaceABI, deployer)
+        const roundData = await priceFeed.latestRoundData()
+        console.log('roundData: ', Number(roundData.answer));
+        const formattedData = Number(roundData.answer) / 100000000 // 40793608215
+        console.log('BNB PRICE is: ', formattedData);
+    } catch (error) {
+        console.log('ERROR while fetching price');
+        console.log(error);
+    }
 }
 
-readPrice()
+readPriceFromAggregator()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error)
